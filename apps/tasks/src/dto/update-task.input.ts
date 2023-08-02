d83d322aa6e99c057d159a415b7cba772528d12d
Enes Buğra Gürbüz <enesbugragurbuz1@gmail.com>
2023-08-02 12:54:11 +0300

    update

--- apps/tasks/src/dto/update-task.input.ts
+++ apps/tasks/src/dto/update-task.input.ts
@@ -1,14 +1,28 @@
-// update-task.input.ts
 import { InputType, Field } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateTaskInput {
   @Field({ nullable: true })
-  title?: string;
+  root_id?: string;
 
   @Field({ nullable: true })
   creator_id?: string;
 
   @Field({ nullable: true })
   project_id?: string;
+
+  @Field({ nullable: true })
+  title?: string;
+
+  @Field({ nullable: true })
+  description?: string;
+
+  @Field({ nullable: true })
+  deadline?: Date;
+
+  @Field({ nullable: true })
+  status?: string;
+
+  @Field({ nullable: true })
+  created_at?: Date;
 }
