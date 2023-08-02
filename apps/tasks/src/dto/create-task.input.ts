098816973df0bb88583b1aa12a33c905c1b14b5c
Enes Buğra Gürbüz <enesbugragurbuz1@gmail.com>
2023-08-02 12:53:41 +0300

    update

--- apps/tasks/src/dto/create-task.input.ts
+++ apps/tasks/src/dto/create-task.input.ts
@@ -1,12 +1,12 @@
 import { InputType, ID, Field } from '@nestjs/graphql';
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Column } from 'typeorm';
 
 @InputType()
 export class CreateTaskInput {
 
   @Column()
   @Field()
-  title: string;
+  root_id: string;
 
   @Column()
   @Field()
@@ -15,4 +15,20 @@ export class CreateTaskInput {
   @Column()
   @Field()
   project_id: string;
+
+  @Column()
+  @Field()
+  title: string;
+
+  @Column()
+  @Field()
+  description: string;
+
+  @Column({ type: 'datetime', nullable: true})
+  @Field({ nullable: true })
+  deadline: Date;
+
+  @Column()
+  @Field()
+  status: string;
 }
